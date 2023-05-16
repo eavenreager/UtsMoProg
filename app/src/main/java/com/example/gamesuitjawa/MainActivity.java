80d1c410cd0688ce9966edfe603370b747adf9f8
Zean <narutoboruto26@gmail.com>
2023-05-16 20:10:53 +0700

    update feature

--- app/src/main/java/com/example/gamesuitjawa/MainActivity.java
+++ app/src/main/java/com/example/gamesuitjawa/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.gamesuitjawa;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     private ImageView player,com;
     private ImageButton btnbatu,btngunting,btnkertas;
     private int scorePlayer=0, scoreKomputer =0;
-
+    private MediaPlayer playersmenang,playerskalah;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,14 +31,17 @@ public class MainActivity extends AppCompatActivity {
         btngunting =findViewById(R.id.btngunting);
         btnkertas=findViewById(R.id.btnkertas);
 
+        playersmenang = MediaPlayer.create(this, R.raw.menang);
+        playerskalah = MediaPlayer.create(this, R.raw.kalah);
+
         btnbatu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 player.setImageResource(R.drawable.batu_bawah);
               String message = gamestart( "batu");
-                Toast.makeText(MainActivity.this,"Kamu Memilih Batu"+message,Toast.
+                Toast.makeText(MainActivity.this,"Kamu Memilih Batu "+message,Toast.
                         LENGTH_SHORT).show();
-                txscore.setText("Score Player:"+Integer.toString(scorePlayer)+"Score Komputer"+
+                txscore.setText("Score Player : "+Integer.toString(scorePlayer)+" Score Komputer : "+
                         Integer.toString(scoreKomputer));
             }
         });
@@ -46,9 +50,9 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 player.setImageResource(R.drawable.gunting_bawah);
                 String message = gamestart( "gunting");
-                Toast.makeText(MainActivity.this,"Kamu Memilih Gunting"+message,Toast.
+                Toast.makeText(MainActivity.this,"Kamu Memilih Gunting "+message,Toast.
                         LENGTH_SHORT).show();
-                txscore.setText("Score Player:"+Integer.toString(scorePlayer)+"Score Komputer"+
+                txscore.setText("Score Player : "+Integer.toString(scorePlayer)+" Score Komputer : "+
                         Integer.toString(scoreKomputer));
             }
         });
@@ -57,12 +61,17 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 player.setImageResource(R.drawable.kertas_bawah);
                String message = gamestart("kertas");
-                Toast.makeText(MainActivity.this,"Kamu Memilih Kertas"+message,Toast.
+                Toast.makeText(MainActivity.this,"Kamu Memilih Kertas "+message,Toast.
                         LENGTH_SHORT).show();
-                txscore.setText("Score Player:"+Integer.toString(scorePlayer)+"Score Komputer"+
+                txscore.setText("Score Player : "+Integer.toString(scorePlayer)+" Score Komputer : "+
                         Integer.toString(scoreKomputer));
             }
         });
+    }
+    protected void onPause() {
+        super.onPause();
+        playersmenang.release();
+        playerskalah.release();
     }
 
     private String gamestart( String player) {
@@ -71,9 +80,9 @@ public class MainActivity extends AppCompatActivity {
 
         int angka_komputer = angka_random.nextInt(3)+1;
 
-        if (angka_komputer ==1){
+        if (angka_komputer == 1){
             komputer="batu";
-        } else if (angka_komputer ==2) {
+        } else if (angka_komputer == 2) {
             komputer="gunting";
         } else if (angka_komputer == 3) {
             komputer ="kertas";
@@ -87,29 +96,35 @@ public class MainActivity extends AppCompatActivity {
         }
 //aturan main
         if (komputer == player){
-            return "SERI";
+            return "Seri";
         }else if(player == "batu" && komputer == "gunting"){
             scorePlayer++;
+            playersmenang.start();
             return "Kamu Menang";
 
         } else if (player == "batu" && komputer == "kertas") {
             scoreKomputer++;
+            playerskalah.start();
             return "Kamu Kalah";
 
         } else if (player == "gunting" && komputer == "kertas") {
             scorePlayer++;
+            playersmenang.start();
             return "Kamu Menang";
 
         } else if (player == "gunting" && komputer == "batu") {
             scoreKomputer++;
+            playerskalah.start();
             return "Kamu Kalah";
 
         } else if (player == "kertas" && komputer == "batu") {
             scorePlayer++;
+            playersmenang.start();
             return "Kamu Menang";
 
         }else {
             scoreKomputer++;
+            playerskalah.start();
             return "Kamu Kalah";
 
         }
